4b911aab043e2c9336aeca5308ed220cc95ce73d
vegueta <omar-11od@gmail.com>
2015-12-26 18:33:40 -0600

    version 3

--- Entronque/Entronque1/views.py
+++ Entronque/Entronque1/views.py
@@ -5,3 +5,9 @@ from django.http import HttpResponse
 # Create your views here.
 def index(request):
     return render(request, 'index.html');
+
+def metodo(request):
+    return render(request, 'index1.html');
+
+
+
