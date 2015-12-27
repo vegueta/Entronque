7412ed91b02943d8b462f03972dfc455decbc3a9
Manuelpineda1284 <manuel_chingon94@hotmail.com>
2015-12-26 18:31:12 -0600

    Version2

--- Entronque/Entronque1/views.py
+++ Entronque/Entronque1/views.py
@@ -5,3 +5,5 @@ from django.http import HttpResponse
 # Create your views here.
 def index(request):
     return render(request, 'index.html');
+def metodo(request):
+    return render(request, 'index.html');
